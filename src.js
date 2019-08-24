25e8af8cfc2cc93082e19d8b6b8deed27f34cfb8
Leonardo Quintana Juarez <30363785+JorgeAntrax@users.noreply.github.com>
2019-08-24 14:04:03 -0600

    Update src.js

--- src.js
+++ src.js
@@ -1,4 +1,31 @@
 class API {
+    constructor() {
+        this.path = '/js';
+    }
+
+    static get($url, $parent = 'body') {
+        fetch($url, {
+                method: 'GET'
+            })
+            .then($r => $r.text())
+            .catch($r => console.log('Not loaded file from CDN'))
+            .then($r => {
+                if ($url.indexOf('js') != -1) {
+                    let $script = document.createElement('script');
+                    $script.setAttribute('dynamic-script', true);
+                    $script.setAttribute('async', true);
+                    $script.innerHTML = $r;
+                    document.querySelector($parent).appendChild($script);
+                } else if ($url.indexOf('css') != -1) {
+                    $css.setAttribute('dynamic-css', true);
+                    $css.innerHTML = $r;
+                    document.querySelector($parent).appendChild($css);
+                } else {
+                    console.error('Get url is not valid, please insert a url type JS or CSS');
+                }
+            });
+    }
+
     static set($file, $module = 'body', $type = 'file') {
         if (Array.isArray($file)) {
             for (let $i = 0; $i < $file.length; $i++) {
@@ -12,7 +39,7 @@ class API {
     static create($script, $module, $type) {
         let s = document.createElement("script");
         if ($type == 'file') {
-            s.src = `/js/${$script}.js`;
+            s.src = `${this.path}/${$script}.js`;
         } else {
             s.src = $script;
         }
